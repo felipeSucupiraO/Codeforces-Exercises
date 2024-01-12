a1528eb5b69d5d750bb83f684edd73918166142d
Aust2000 <felipeuberaba10@gmail.com>
2024-01-12 00:13:02 -0300

    exercise done

--- exercises/notquietlatinsquare.c
+++ exercises/notquietlatinsquare.c
@@ -7,19 +7,25 @@
 
 int main() {
     int numInputs;
-    scanf("%d", &numInputs);
+    scanf("%d\n", &numInputs);
 
     char matrix[3][3];
     int unknownLine;
+    int lineSum;
+    int unknownLetter;
     while (numInputs--) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                scanf("%c", &matrix[i][j]);
+                scanf(" %c", &matrix[i][j]);
                 if (matrix[i][j] == '?') {
                     unknownLine = i;
                 }
             }
         }
+
+        lineSum = matrix[unknownLine][0] + matrix[unknownLine][1] + matrix[unknownLine][2] - '?';
+        unknownLetter = 'A' + 'B' + 'C' - lineSum;
+        printf("%c\n", unknownLetter);
     }
     
     return 0;
