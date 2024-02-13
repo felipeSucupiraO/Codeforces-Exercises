436128943df9b6b6a9cbff237344e0a7009bc6c0
Felipe Sucupira <58998427+Aust2000@users.noreply.github.com>
2024-02-13 03:58:01 +0000

    exercise completed

--- exercises/unnaturalleanguageprocessing.c
+++ exercises/unnaturalleanguageprocessing.c
@@ -10,14 +10,61 @@
 // For example, given the word bacedbab, it would be split into syllables as ba.ced.bab (the dot . represents a syllable boundary).
 
 #include <stdio.h>
+#include <stdlib.h>
+
+char *alocateString(int size) {
+    char *pointer = NULL;
+    do {
+        pointer = (char *)malloc(sizeof(char) * size);
+    } while (pointer == NULL);
+    return pointer;
+}
+
+int isVowel(char letter) {
+    if (letter == 'a' || letter == 'e') {
+        return 1;
+    }
+    return 0;
+}
+
+char *wordSeparatedBySyllables(char *word, int wordLength) {
+    char *resultWord = alocateString(wordLength + (wordLength / 2));
+    int currResultIndex = 0;
+    
+    for (int i = 0; i < wordLength; i++) {
+        resultWord[currResultIndex++] = word[i];
+        if (word[i + 1] == '\0') {
+            break;
+        } else if ((isVowel(word[i]) && isVowel(word[i + 2])) || (!isVowel(word[i]) && !isVowel(word[i + 1]))) {
+            resultWord[currResultIndex++] = '.';
+            wordLength++;
+        }
+    }
+
+    resultWord[currResultIndex] = '\0';
+
+    return resultWord;
+}
+
 
 int main() {
-    int quantidadeInputs;
-    scanf("%d", &quantidadeInputs);
+    int amountInputs;
+    scanf("%d", &amountInputs);
 
-    while (quantidadeInputs--) {
-        // i was thinking on how to identify the different syllables
-        // I think I need to create a new array containing only Cs and Vs accordingly to the input
+    int wordLength;
+    char *word;
+    char *resultWord;
+    while (amountInputs--) {
+        scanf("%d%*c", &wordLength);
+        
+        word = alocateString(wordLength + (wordLength / 2));
+        scanf("%[^\n]%*c", word);
+        
+        resultWord = wordSeparatedBySyllables(word, wordLength);
+        printf("%s\n", resultWord);
+
+        free(word);
+        free(resultWord);
     }
     
     return 0;
